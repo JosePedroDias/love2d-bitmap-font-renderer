da142c83f4c71c9fe8ce35debf89f18eead1cc7c
José Pedro Dias <jose.pedro.dias@gmail.com>
2016-01-18 04:08:31 +0000

    works of chrome

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 A [bitmap font renderer](https://love2d.org/wiki/love.graphics.newImageFont) for [love2d](https://love2d.org/) 0.10 using canvas and JS.
 
-For the time being, works on Firefox only.  
+Worked fine in Firefox from day 1. Now works in Chrome too, after some serious hacking.
 If you get sans-serif please refresh the page O:)
 
 The font used in the example comes from here:  
 <https://www.behance.net/gallery/31268855/Determination-Better-Undertale-Font>
 
-Right click > Save image as...
+## What it does
+
+```javascript
+var s = 2;
+bitmapFontRenderer({
+    font: 'DeterminationMonoWeb',
+    ext: 'woff',
+    size: 20*s,
+    color: '#FFF',
+    splitColor: '#F0F',
+    splitW: s,
+    gap: s,
+    characters: ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.!?;:-_/|\\!\'"+*()[]{}&%$#@',
+    //roundTo: 64,
+    draw: function(c, ch, x, y, o) {
+        c.fillStyle = '#00F';
+        c.fillText(ch, x-s, y);
+        c.fillText(ch, x+s, y);
+        c.fillText(ch, x, y-s);
+        c.fillText(ch, x, y+s);
+        c.fillStyle = o.color;
+        c.fillText(ch, x, y);
+    }
+});
+```
+
+and you get...
+
+![bitmap font result](DeterminationMonoWeb.png)
+
+Use **Right click > Save image as...** to save it
+
 
 ## Params
 
--- bitmapFontRenderer.js
+++ bitmapFontRenderer.js
@@ -5,93 +5,98 @@ function round(n, mult) {
 function bitmapFontRenderer(o) {
     'use strict';
 
-    //var styleEl = document.head.querySelector('style');
-    //styleEl.firstChild.nodeValue = styleEl.firstChild.nodeValue
-    document.styleSheets[0].insertRule(
-        "@font-face { font-family: '" + o.font + "'; src: url('" + o.font + "." + o.ext + "') format('" + o.ext + "'); }"
-    , 0);
-
-    var el = document.createElement('canvas');
-
-    el.mozImageSmoothingEnabled = false;
-	el.webkitImageSmoothingEnabled = false;
-	el.imageSmoothingEnabled = false;
-
-    el.setAttribute('width', o.size * o.characters.length);
-    el.setAttribute('height', o.size + o.gap*2);
-    document.body.appendChild(el);
-
-    var c = el.getContext('2d');
-    c.font = o.size + 'px ' + o.font;
-    c.textBaseline = 'top';
-
-    var x = 0;
-    var dx = o.splitW;
-    var gap = o.gap;
-    var y = gap;
-    var chars = o.characters.split('');
-
-
-    // font rendering pass
-    if (o.color) {
-        x += dx;
-
-        chars.forEach(function(ch) {
-            x += gap;
-
-            c.fillStyle = o.color;
-
-            if (o.draw) {
-                o.draw(c, ch, x, y, o);
-            }
-            else {
-                c.fillText(ch, x, y);
-            }
-
-            var w = c.measureText(ch).width;
-
-            x += gap + w + dx;
-        });
+    // VERY HACKY STUFF SO CHROME PLAYS NICE
+    var img = new Image();
+    img.src = o.font + '.' + o.ext;
+    img.onerror = function() {
+        var testerEl = document.querySelector('#woff-tester');
+        testerEl.parentNode.removeChild(testerEl);
+        letsGo();
     }
 
+    function letsGo() {
+        var el = document.createElement('canvas');
 
-    // split overlay pass
-    if (o.splitColor) {
-        x = 0;
+        el.mozImageSmoothingEnabled = false;
+    	el.webkitImageSmoothingEnabled = false;
+    	el.imageSmoothingEnabled = false;
 
-        c.fillStyle = o.splitColor;
-        c.fillRect(x, y-gap, dx, o.size+gap*2);
-        x += dx;
+        el.setAttribute('width', o.size * o.characters.length);
+        el.setAttribute('height', o.size + o.gap*2);
+        document.body.appendChild(el);
 
-        chars.forEach(function(ch) {
-            x += gap;
+        var c = el.getContext('2d');
+        c.font = o.size + 'px ' + o.font;
+        c.textBaseline = 'top';
 
-            var w = c.measureText(ch).width;
+        var x = 0;
+        var dx = o.splitW;
+        var gap = o.gap;
+        var y = gap;
+        var chars = o.characters.split('');
 
-            x += gap;
+
+        // font rendering pass
+        if (o.color) {
+            x += dx;
+
+            chars.forEach(function(ch) {
+                x += gap;
+
+                c.fillStyle = o.color;
+
+                if (o.draw) {
+                    o.draw(c, ch, x, y, o);
+                }
+                else {
+                    c.fillText(ch, x, y);
+                }
+
+                var w = c.measureText(ch).width;
+
+                x += gap + w + dx;
+            });
+        }
+
+
+        // split overlay pass
+        if (o.splitColor) {
+            x = 0;
 
             c.fillStyle = o.splitColor;
-            c.fillRect(x+w, y-gap, dx, o.size+gap*2);
+            c.fillRect(x, y-gap, dx, o.size+gap*2);
+            x += dx;
 
-            x += w + dx;
-        });
+            chars.forEach(function(ch) {
+                x += gap;
+
+                var w = c.measureText(ch).width;
+
+                x += gap;
+
+                c.fillStyle = o.splitColor;
+                c.fillRect(x+w, y-gap, dx, o.size+gap*2);
+
+                x += w + dx;
+            });
+        }
+
+
+        // trim
+        var el2 = document.createElement('canvas');
+
+        var W = x;
+        var H = o.size + o.gap*2;
+        if (o.roundTo) {
+            W = round(W, o.roundTo);
+            H = round(H, o.roundTo);
+        }
+
+        el2.setAttribute('width', W);
+        el2.setAttribute('height', H);
+        var c2 = el2.getContext('2d');
+        c2.drawImage(el, 0, 0);
+        document.body.removeChild(el);
+        document.body.appendChild(el2);
     }
-
-
-    // trim
-    var el2 = document.createElement('canvas');
-
-    var W = x;
-    var H = o.size + o.gap*2;
-    if (o.roundTo) {
-        W = round(W, o.roundTo);
-        H = round(H, o.roundTo);
-    }
-
-    el2.setAttribute('width', W);
-    el2.setAttribute('height', H);
-    var c2 = el2.getContext('2d');
-    c2.drawImage(el, 0, 0);
-    document.body.removeChild(el);
-    document.body.appendChild(el2);
 }
